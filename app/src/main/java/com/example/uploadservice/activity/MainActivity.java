4019348be37b3ff36642f307c9c68847c81a6f88
kb18519142009 <847584597@qq.com>
2018-03-15 16:17:46 +0800

    Initial commit

---
+++ app/src/main/java/com/example/uploadservice/activity/MainActivity.java
@@ -0,0 +1,97 @@
+package com.example.uploadservice.activity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.uploadservice.R;
+import com.example.uploadservice.upload.ProgressRequestBody;
+
+import java.io.File;
+
+import okhttp3.MultipartBody;
+import retrofit2.Call;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG = "MainActivity";
+
+    private Button mUploadPicture;
+    private Button mUploadVideo;
+
+    private Context mContext;
+
+    private String mPicPath;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mContext = this;
+
+        TextView toolbarTitle = findViewById(R.id.tv_toolbar_title);
+        toolbarTitle.setText(R.string.main_activity_title);
+        mUploadPicture = findViewById(R.id.btn_upload_picture);
+        mUploadPicture.setOnClickListener(this);
+        mUploadVideo = findViewById(R.id.btn_upload_video);
+        mUploadVideo.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mUploadPicture) {
+
+        } else if (v == mUploadVideo) {
+
+        }
+    }
+
+    private void uploadPicture() {
+        File file = new File(mPicPath);
+
+        //实现上传进度监听
+        ProgressRequestBody requestFile = new ProgressRequestBody(file, "image/*", new ProgressRequestBody.UploadCallbacks() {
+            @Override
+            public void onProgressUpdate(int percentage) {
+                Log.e(TAG, "onProgressUpdate: " + percentage);
+            }
+
+            @Override
+            public void onError() {
+
+            }
+
+            @Override
+            public void onFinish() {
+            }
+        });
+
+        MultipartBody.Part body =
+                MultipartBody.Part.createFormData("file", file.getName(), requestFile);
+
+//        mApiV1.uploadFile(body).enqueue(new RespCallback<UploadVideoResp>(mContext) {
+//            @Override
+//            public void onResp(@NonNull Call<UploadVideoResp> call, @NonNull Response<UploadVideoResp> response) {
+//
+//                UploadVideoResp resp = response.body();
+//                if (resp != null) {
+//                    submitFeedback(resp.getResult());
+//                }
+//            }
+//
+//            @Override
+//            public void onFail(@NonNull Call<UploadVideoResp> call, @NonNull Throwable t) {
+//                Log.e(TAG, "onFail: " + t.toString());
+//                Toast.makeText(mContext, "上传失败，稍后重试", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+    }
+}
---
+++ app/src/main/java/com/example/uploadservice/upload/ProgressRequestBody.java
@@ -0,0 +1,90 @@
+package com.example.uploadservice.upload;
+
+import android.os.Handler;
+import android.os.Looper;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import okhttp3.MediaType;
+import okhttp3.RequestBody;
+import okio.BufferedSink;
+
+/**
+ * Description：
+ * Created by kang on 2018/3/13.
+ */
+
+public class ProgressRequestBody extends RequestBody {
+    private File mFile;
+    private String mPath;
+    private String mMediaType;
+    private UploadCallbacks mListener;
+
+    private int mEachBufferSize = 1024;
+
+    public interface UploadCallbacks {
+        void onProgressUpdate(int percentage);
+
+        void onError();
+
+        void onFinish();
+    }
+
+    public ProgressRequestBody(final File file, String mediaType, final UploadCallbacks listener) {
+        mFile = file;
+        mMediaType = mediaType;
+        mListener = listener;
+    }
+
+    public ProgressRequestBody(final File file, String mediaType, int eachBufferSize, final UploadCallbacks listener) {
+        mFile = file;
+        mMediaType = mediaType;
+        mEachBufferSize = eachBufferSize;
+        mListener = listener;
+    }
+
+    @Override
+    public MediaType contentType() {
+        // i want to upload only images
+        return MediaType.parse(mMediaType);
+    }
+
+    @Override
+    public void writeTo(BufferedSink sink) throws IOException {
+        long fileLength = mFile.length();
+        byte[] buffer = new byte[mEachBufferSize];
+        FileInputStream in = new FileInputStream(mFile);
+        long uploaded = 0;
+
+        try {
+            int read;
+            Handler handler = new Handler(Looper.getMainLooper());
+            while ((read = in.read(buffer)) != -1) {
+                // update progress on UI thread
+                handler.post(new ProgressUpdater(uploaded, fileLength));
+                uploaded += read;
+                sink.write(buffer, 0, read);
+
+            }
+        } finally {
+            in.close();
+        }
+    }
+
+    private class ProgressUpdater implements Runnable {
+        private long mUploaded;
+        private long mTotal;
+
+        public ProgressUpdater(long uploaded, long total) {
+            mUploaded = uploaded;
+            mTotal = total;
+        }
+
+        @Override
+        public void run() {
+            mListener.onProgressUpdate((int) (100 * mUploaded / mTotal));
+        }
+    }
+}
