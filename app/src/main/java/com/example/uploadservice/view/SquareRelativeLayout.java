7157b247286a408b891439cb8a5b8b7effe8a28f
kb18519142009 <847584597@qq.com>
2018-03-16 16:05:30 +0800

    添加squareLayout

---
+++ app/src/main/java/com/example/uploadservice/view/SquareRelativeLayout.java
@@ -0,0 +1,31 @@
+package com.example.uploadservice.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+/**
+ * SquareRelativeLayout
+ * Created by kang on 2017/10/26.
+ */
+public class SquareRelativeLayout extends RelativeLayout {
+
+    public SquareRelativeLayout(Context context) {
+        super(context);
+    }
+
+    public SquareRelativeLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SquareRelativeLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        // Set a square layout.
+        super.onMeasure(widthMeasureSpec, widthMeasureSpec);
+    }
+
+}
